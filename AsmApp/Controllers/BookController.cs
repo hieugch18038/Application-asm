232291d266e85befed615acb49fe709cbae2ad8b
hieugch18038 <hieunhgch18038@fpt.edu.vn>
2022-08-18 11:54:53 +0700

    Update database

---
+++ AsmApp/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using AsmApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AsmApp.Controllers
+{
+    public class BookController : Controller
+    {
+        //khai báo ApplicationDbContext để truy xuất và thay đổi dữ liệu của bảng
+        private ApplicationDbContext context;
+        public BookController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var books = context.Book.ToList();
+            return View(books);
+        }
+        public IActionResult List()
+        {
+            return View(context.Book.ToList());
+        }
+    }
+}
